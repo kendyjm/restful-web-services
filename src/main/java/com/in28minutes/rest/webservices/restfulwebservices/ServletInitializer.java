ea411c2060910fe5d1130a22949b581ced60e16e
Kendy <kendyjm@gmail.com>
2020-04-12 09:31:54 +0200

    initial commit

---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.in28minutes.rest.webservices.restfulwebservices;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(RestfulWebServicesApplication.class);
+	}
+
+}
