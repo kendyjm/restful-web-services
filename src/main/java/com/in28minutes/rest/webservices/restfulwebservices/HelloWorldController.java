ba2c1e17798a34d256cd878bce301bceb2f1afae
Kendy <kendyjm@gmail.com>
2020-04-12 14:24:54 +0200

    13. Step 02 - Understanding the RESTful Services we would create in this course of Section 3 : Restful Web Services with Spring Boot

---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -0,0 +1,20 @@
+package com.in28minutes.rest.webservices.restfulwebservices;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    //GET
+    //URI - /hello-world
+    //method - "Hello World"
+    //@RequestMapping(method = RequestMethod.GET, path = "/hello-world")
+    @GetMapping(path = "/hello-world")
+    public String helloWorld() {
+        return "Hello World!";
+    }
+}
