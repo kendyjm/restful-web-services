de5e101f86f5720500b35096181d309d2b173a43
Kendy <kendyjm@gmail.com>
2020-04-13 14:10:19 +0200

    22. Step 11 - Implementing Exception Handling - 404 Resource Not Found of Section 3 : Restful Web Services with Spring Boot

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
@@ -22,8 +22,14 @@ public class UserResource {
 
     // GET /users/{id}
     @GetMapping(path = "/users/{id}")
-    public User retrieveUser(@PathVariable int id) {
-        return service.findOne(id);
+    public ResponseEntity<User> retrieveUser(@PathVariable int id)
+    {
+        User user = service.findOne(id);
+        if ( user == null ) {
+            throw new UserNotFoundException("id-"+id); // see @ResponseStatus(HttpStatus.NOT_FOUND) in this class
+        }
+
+        return ResponseEntity.ok(user);
     }
 
     // CREATED
