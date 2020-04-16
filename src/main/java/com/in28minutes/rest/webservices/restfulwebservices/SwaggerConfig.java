3026c9905bc1235d842182a3131a16f605f3a8e9
Kendy <kendyjm@gmail.com>
2020-04-16 15:37:07 +0200

    37. Step 22 - Enhancing Swagger Documentation with Custom Annotations of Section 3 : Restful Web Services with Spring Boot

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/SwaggerConfig.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/SwaggerConfig.java
@@ -2,15 +2,27 @@ package com.in28minutes.rest.webservices.restfulwebservices;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.*;
+
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+    public static final Contact DEFAULT_CONTACT = new Contact("Kendy", "www.myurl.com", "kendyjm@gmail.com");
+    private static final ApiInfo DEFAULT_API_INFO = new ApiInfo("My API Documentation", "My Api description", "1.0", "urn:tos2",DEFAULT_CONTACT, "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0", new ArrayList());
+    private static final Set<String> PRODUCES_CONSUMES_DEFAULT = new HashSet<String>(Arrays.asList(MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE));
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2);
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(DEFAULT_API_INFO)
+                .consumes(PRODUCES_CONSUMES_DEFAULT)
+                .produces(PRODUCES_CONSUMES_DEFAULT)
+                ;
     }
 }
--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
@@ -1,21 +1,29 @@
 package com.in28minutes.rest.webservices.restfulwebservices.user;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiParam;
+
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
+@ApiModel(value = "My description of User")
 public class User {
     private Integer id;
 
     @Size(min = 2, message = "Name should have at least 2 characters")
+    @ApiModelProperty(value = "Name should have at least 2 characters")
     private String name;
 
     @Past
+    @ApiModelProperty(value = "Birth date should be in the past")
     private Date birthDate;
 
     private User() {}
 
-    public User(Integer id, String name, Date birthDate) {
+
+    public User(@ApiParam(value = "My Id") Integer id, String name, Date birthDate) {
         this.id = id;
         this.name = name;
         this.birthDate = birthDate;
